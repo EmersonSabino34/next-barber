959e87a5c0c677e190c3b8c3994591a35cf160f6
emerson sabino <emersonsabino34@gmail.com>
2025-05-20 22:28:51 +0100

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,20 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { Menu } from "lucide-react";
+import { Menu, Scissors } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="lg:w-screen w-[100%] h-screen bg-black overflow-hidden">
-      <nav className="bg-zinc-900 p-7 flex justify-between items-center">
+      <nav className="bg-zinc-900 p-4 flex justify-between items-center">
 
-      <Avatar>
-        <AvatarImage src="https://avatars.githubusercontent.com/u/168974048?v=4" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-
-      <h1 className="text-slate-700 font-semibold lg:text-xl text-md">Emerson Sabino developer</h1>
+      <div className=" w-44 h-7">
+        <div className="flex items-center font-bold">
+        <Scissors className="text-purple-700" />
+        <h3 className="text-purple-700 text-2xl">SHOP</h3>
+        <h3 className="text-white text-2xl">BARBER</h3>
+        </div>
+      </div>
 
       <Sheet>
         <SheetTrigger><Menu className="text-white border p-1 rounded-sm" /></SheetTrigger>
