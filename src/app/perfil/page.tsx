1f534660873d5db4038b688dfd2f5368cf41e8d2
emerson sabino <emersonsabino34@gmail.com>
2025-05-20 22:32:08 +0100

    updated

---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,7 @@
+export default function Perfil(){
+    return(
+        <div className="">
+            <h3 className="text-6xl">Hello Perfil</h3>
+        </div>
+    )
+}
---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,7 @@
+export default function Produtos(){
+    return(
+        <div>
+            <h1 className="text-6xl">Hello Produtos</h1>
+        </div>
+    )
+}
